7ee3dd991fe2b772a55b261ad878816407f57452
Balaji Nadar <balajigittttt@gmail.com>
2026-07-26 05:44:54 +0530

    Made a simple layout

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,24 @@
 import "./globals.css";
+import CustomCursor from "./components/CustomCursor";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export const metadata = {
-  title: "CSI 2026-27",
-  description: "CSI 2026-27",
+  title: "CSI SFIT 2026-27",
+  description: "CSI SFIT 2026-27",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className="scroll-smooth">
+      <body className="bg-black text-white overflow-x-hidden">
+        <CustomCursor />
+        <Navbar />
+
+        {children}
+
+        <Footer />
+      </body>
     </html>
   );
 }
